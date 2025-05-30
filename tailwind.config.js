9165ed52e325e9fe97c6949bf84805b3e556c1e7
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-05-30 21:09:43 +0100

    Create base for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B0A26",
+        secondary: "#131240",
+        complementary: "#8563A6",
+      },
+    },
+  },
+  plugins: [],
+};
